b0c7720bd7cc3551782d94ef64d8e953dab88dc6
Drake B <drakeboard719@riversideunified.org>
2023-11-16 14:24:07 -0800

    On Lab 4.5

--- Lab Project/Assets/Scripts/PlayerController.cs
+++ Lab Project/Assets/Scripts/PlayerController.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    private float speed = 10f;
+    private float speed = 300f;
     private Rigidbody playerRb;
     private float zBound = 6;
 
@@ -43,6 +43,22 @@ public class PlayerController : MonoBehaviour
         if (transform.position.z > zBound)
         {
             transform.position = new Vector3(transform.position.x, transform.position.y, zBound);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.CompareTag("Enemy"))
+        {
+            Debug.Log("Player has collided with enemy.");
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Powerup"))
+        {
+            Destroy(other.gameObject);
         }
     }
 }
